58a20b9095aeeb217743cd85fb5cabebf6ab41f0
shigilab <shirvani.abddullayev@gmail.com>
2020-03-10 01:59:03 +0100

    Create GetProcessID.cpp

---
+++ GetProcessID.cpp
@@ -0,0 +1,20 @@
+DWORD GetProcessId(const wchar_t* procName) {
+    DWORD procID = 0;
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnap!=INVALID_HANDLE_VALUE)    
+    {
+        PROCESSENTRY32 procEntry;
+        procEntry.dwSize = sizeof(PROCESSENTRY32);
+        if (Process32First(hSnap, &procEntry)) {
+            do
+            {
+                if (!_wcsicmp(procEntry.szExeFile,procName)) {
+                    procID = procEntry.th32ProcessID;
+                    break;
+                }
+            } while (Process32Next(hSnap,&procEntry));
+        }
+    }
+    CloseHandle(hSnap);
+    return procID;
+}
